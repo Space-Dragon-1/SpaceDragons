9b22f5692a9aeced5fe700b6cae9601a3ae0d800
Javier Gomez <javiandgo@gmail.com>
2022-10-23 12:41:52 -0500

    Ajuste de error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <main>
         <h1>Productos destacados</h1>
         {data.products.map((product) => (
-          <div>
+          <div key={product.slug}>
             <img src={product.image} alt={product.name} />
             <p>{product.name}</p>
             <p>{product.price}</p>
