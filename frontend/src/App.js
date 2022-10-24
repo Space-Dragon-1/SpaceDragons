84ccc645acda535683b59649d180f7a742c07865
Javier Gomez <javiandgo@gmail.com>
2022-10-24 11:07:38 -0500

    Productos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,13 +8,24 @@ function App() {
       </header>
       <main>
         <h1>Productos destacados</h1>
-        {data.products.map((product) => (
-          <div key={product.slug}>
-            <img src={product.image} alt={product.name} />
-            <p>{product.name}</p>
-            <p>{product.price}</p>
-          </div>
-        ))}
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Agregar al carrito</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
