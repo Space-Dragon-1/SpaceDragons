04dedbb95ffb002d443e09b3e5197cd08a904323
Javier Gomez <javiandgo@gmail.com>
2022-10-25 18:13:03 -0500

    Montaje de servidor sencillo de node

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,28 @@
 import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import 'css/bootstrap.min.css';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Space Shop</a>
-      </header>
-      <main>
-        <h1>Productos destacados</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Agregar al carrito</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Space Shop</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/cart" element={<CartScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+
+          <Link to="/cart">Carrito</Link>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
