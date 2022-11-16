f3016f8da66300c0c81090b708999204a0b8e72e
Javier Gomez <javiandgo@gmail.com>
2022-11-16 17:51:18 -0500

    login

--- backend/server/routes/userRoutes.js
+++ backend/server/routes/userRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
 import User from '../models/userModel';
 
 const userRouter = express.Router();
@@ -7,5 +8,13 @@ userRouter.post(
   '/login',
   expressAsyncHandler(async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      }
+    }
   })
 );
